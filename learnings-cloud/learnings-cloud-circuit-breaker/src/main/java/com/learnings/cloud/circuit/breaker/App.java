ba3760470204926eddaff0f536d4dbb0756826e6
Atul Sharma <s.agrawal8915@gmail.com>
2020-07-23 21:22:24 +0530

    adding projects for cloud

---
+++ learnings-cloud/learnings-cloud-circuit-breaker/src/main/java/com/learnings/cloud/circuit/breaker/App.java
@@ -0,0 +1,13 @@
+package com.learnings.cloud.circuit.breaker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ learnings-cloud/learnings-cloud-kubernetes/src/main/java/com/learnings/cloud/kubernetes/App.java
@@ -0,0 +1,13 @@
+package com.learnings.cloud.kubernetes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
